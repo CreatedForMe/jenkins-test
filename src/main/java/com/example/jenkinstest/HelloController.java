140f54a59096c2fc9b91f9c946de042df540f3df
chchlee <yourfuck@naver.com>
2023-12-22 01:35:07 +0900

    test

---
+++ src/main/java/com/example/jenkinstest/HelloController.java
@@ -0,0 +1,29 @@
+/**
+ * packageName :  com.example.jenkinstest
+ * fileName : HelloController
+ * author :  ichunghui
+ * date : 12/22/23
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 12/22/23                ichunghui             최초 생성
+ */
+
+package com.example.jenkinstest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("hello", "hello-jenkins");
+
+        return "hello";
+    }
+}
+
